e63bd107219820fbde1b7354b8dbc0e74d4f435a
Yoru <rickygogoi500@gmail.com>
2023-06-08 20:10:13 +0530

    formatted code

--- chatgpt.py
+++ chatgpt.py
@@ -12,24 +12,24 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_key = os.environ['apiID']
-model = 'gpt-3.5-turbo'
+api_key = os.environ["apiID"]
+model = "gpt-3.5-turbo"
 print("Chatgpt enterned the chat!")
 while True:
     try:
         query = input("You: ")
-        if query == 'exit':
+        if query == "exit":
             break
         response = openai.ChatCompletion.create(
             api_key=api_key,
             model=model,
-            messages = [
-                {'role' : 'system', 'content' : 'Say hello to chatgpt!'},
-                {'role' : 'user', 'content' : query},
-            ]
+            messages=[
+                {"role": "system", "content": "Say hello to chatgpt!"},
+                {"role": "user", "content": query},
+            ],
         )
         print(f"Chatgpt: {response['choices'][0]['message']['content']}")
     except Exception as e:
         print(f"Error: {e}")
 
-print('Quiting')
+print("Quiting")
